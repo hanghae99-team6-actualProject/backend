8f4df7e69b77674ecf12bce5289976ee81337035
양주혁 <joohyuk.yang00@gmail.com>
2021-11-02 19:49:33 +0900

    양주혁 : 해빗 트래커(당월기준), 그래프(최근1년) 적용 및 api분리

--- controllers/main.js
+++ controllers/main.js
@@ -2,6 +2,13 @@ const { Routine, Action, User, Character, Sequelize } = require("../models");
 const myError = require("./utils/httpErrors");
 const Op = Sequelize.Op;
 
+const today = new Date();
+const year = today.getFullYear(); // 년도
+const month = today.getMonth(); // 월
+const date = today.getDate();  // 날짜
+const fromToday = new Date(year,month,1,0,0,0);
+const fromYearAgo = new Date(year-1,month,date,0,0,0);
+
 //메인 루틴, 액션, 유저 조회
 const ongoingGet = async (req, res, next) => {
   console.log("routineGet router 진입");
@@ -31,8 +38,7 @@ const ongoingGet = async (req, res, next) => {
   }
 };
 
-const historyGet = async (req, res, next) => {
-  console.log("routineGet router 진입");
+const trackerHistoryGet = async (req, res, next) => {
   if (!res.locals.user) return next(myError(401, '로그인되어있지 않습니다'))
   const { id } = res.locals.user;
   const authId = id
@@ -42,7 +48,8 @@ const historyGet = async (req, res, next) => {
       where: {
         userId: authId,
         findate: {
-          [Op.not]: null
+          [Op.not]: null,
+          [Op.gte]: fromToday
         }
       },
     });
@@ -50,19 +57,53 @@ const historyGet = async (req, res, next) => {
       where: {
         userId: authId,
         findate: {
-          [Op.not]: null
+          [Op.not]: null,
+          [Op.gte]: fromToday
         }
       },
     });
-    res.status(200).send({ result: true, finRoutines, finActions, msg: "히스토리 루틴 및 액션 조회완료" });
+    res.status(200).send({ result: true, finRoutines, finActions, msg: "해빗트래커 히스토리 루틴 및 액션 조회완료" });
 
   } catch (err) {
     console.log(err);
-    return next(myError(400, "히스토리 정보 조회 에러 발생"));
+    return next(myError(400, "해빗트래커 히스토리 정보 조회 에러 발생"));
+  }
+};
+
+const graphHistoryGet = async (req, res, next) => {
+  if (!res.locals.user) return next(myError(401, '로그인되어있지 않습니다'))
+  const { id } = res.locals.user;
+  const authId = id
+
+  try {
+    const finRoutines = await Routine.findAll({
+      where: {
+        userId: authId,
+        findate: {
+          [Op.not]: null,
+          [Op.gte]: fromYearAgo
+        }
+      },
+    });
+    const finActions = await Action.findAll({
+      where: {
+        userId: authId,
+        findate: {
+          [Op.not]: null,
+          [Op.gte]: fromYearAgo
+        }
+      },
+    });
+    res.status(200).send({ result: true, finRoutines, finActions, msg: "그래프 히스토리 루틴 및 액션 조회완료" });
+
+  } catch (err) {
+    console.log(err);
+    return next(myError(400, "그래프 히스토리 정보 조회 에러 발생"));
   }
 };
 
 module.exports = {
   ongoingGet,
-  historyGet
+  trackerHistoryGet,
+  graphHistoryGet
 };
