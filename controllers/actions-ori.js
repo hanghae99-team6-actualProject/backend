38e94e2f8dd6b1d5f69838ff36553ad59d1dee44
Andrew <seongchan24@gmail.com>
2021-11-04 19:05:43 +0900

    임성찬: RoutineFin, ActionFin 리셋상황에 대한 처리 완료

--- controllers/actions-ori.js
+++ controllers/actions-ori.js
@@ -194,150 +194,159 @@ const doneAction = async (req, res, next) => {
     if (!res.locals.user) return next(myError(401, '로그인되어있지 않습니다'));
     const userId = res.locals.user.id;
     const { actionId, routineId, isReset } = req.body;
-
+    
     console.log('userId', userId);
     console.log('actionId', actionId);
     console.log(isReset);
-
+    
     let finDate = new Date();
 
-    if (isReset == 1) {
-      console.log('리셋 true, 리셋 실행');
-      // 리셋1. 바꿔줘야할 대상을 찾는다
-      const target = await Action.findAll({
-        where: {
-          routineId,
-          userId,
-        },
-        include: [
-          {
-            model: ActionFin,
-            attributes: ['date'],
-          },
-        ],
-      }).catch((err) => {
-        if (err) return next(new Error('리셋 대상 찾기 db 에러'));
-      });
-
-      // console.log("타겟 액션들의 정보", target);
-      // console.log("타겟 액션의 액션Id값", target[0].id);
-
-      console.log('target의 fin data null로 변환 시작');
-      for (let i = 0; i < target.length; i++) {
-        await ActionFin.update(
-          { date: null },
-          { where: { actionId: target[i].id } }
-        )
-          .then((result) => {
-            console.log(
-              '액션id' + target[i].id + '의 액션 fin 데이터 업데이트 완료'
-            );
-          })
-          .catch((err) => {
-            if (err) return next(new Error('db 에러'));
-          });
-      }
-      return res.send({
-        result: 'true5',
-        msg: 'ActionFin data 리셋에 성공했습니다.',
-      });
-    } else if (isReset == 0) {
-
-      // 2. 유저정보와 액션이 정말 일치하는 데이터인지 확인
-    const action = await Action.findOne({
+    // 2. 유저정보와 액션이 정말 일치하는 데이터인지 확인
+    await Action.findOne({
       where: { id: actionId },
       include: [
         {
           model: ActionFin,
         },
       ],
-    }).catch((err) => {
-      if (err) return next(new Error('db 에러'));
-    });
+    }).then( async (action) => {
+      console.log('여기 오긴하냐?');
+      console.log(isReset);
+      // isReset이 true 라면, 만들었던 루틴안의 액션의 findate를 다시 null값으로 수정해야한다.
+      if (isReset === false) {
+        console.log("리셋 false, 비실행. 다음단계로")
 
-      // isReset === false 일때, 무조건 해당 액션은 findate가 null이어야 한다.
-      if (action.ActionFins[0].date !== null && action.ActionFins[0].date) {
-        return next(new Error('이미 완료된 액션인데 왜 들어왔을까요?'));
-      }
-    }
+        // console.log('액션 db 검색', action);
+        // console.log('액션fin의 date db 검색', action.ActionFins[0].date);
 
-    //3. 액션에 맞는 ActionFin의 실 데이터 생성
-    console.log('setActionFinDate 진입');
-    await setActionFinDate(actionId, finDate).catch((err) => {
-      if (err) next(new Error('setActionFinDate db 에러'));
-    });
+        //잘못된 액션이 입력된 경우 throws
+        // 리셋을 하는 것이 아니라면 date를 보유하고 있으면 안됌
+        if (action.ActionFins[0].date !== null && action.ActionFins[0].date) {
+          return next(new Error('이미 완료된 액션인데 왜 들어왔을까요?'));
+        }
 
-    //4. ActionFin에 data값이 생성되지 않은 ActionId을 추출해야함
-    const target = await Routine.findAll({
-      where: { id: routineId },
-      include: [
-        {
-          model: Action,
+      } else {
+        console.log("리셋 true, 리셋 실행")
+        // 리셋1. 바꿔줘야할 대상을 찾는다
+        const target = await Action.findAll({
+          where: {
+            routineId,
+            userId,
+          },
           include: [
             {
-              model: ActionFin,
+              model:ActionFin,
+              attributes : ['date'],
+            }
+          ]
+        }).catch((err) => { if (err) return next(new Error('리셋 대상 찾기 db 에러')) });
+
+        // console.log("타겟 액션들의 정보", target);
+        // console.log("타겟 액션의 액션Id값", target[0].id);
+
+        console.log('target의 fin data null로 변환 시작')
+        for (let i=0; i<target.length; i++ ){
+          await ActionFin.update({date: null}, {where: {actionId: target[i].id}}).then((result) => {
+            console.log('액션id' + target[i].id + '의 액션 fin 데이터 업데이트 완료');
+          })
+        }
+        return res.send({
+          result: true,
+          msg: "ActionFin data 리셋에 성공했습니다.",
+        });
+      }
+    })
+    
+
+
+      //3. 액션에 맞는 ActionFin의 실 데이터 생성
+      .then(async () => {
+        console.log('setActionFinDate 진입');
+        await setActionFinDate(actionId, finDate).catch((err) => {
+          if (err) next(new Error('setActionFinDate db 에러'));
+        });
+      })
+
+      //4. ActionFin에 data값이 생성되지 않은 Action Id 검색
+      // >> 바뀌어야 함. 루틴안에 들어있는 액션 id들로 추적해야함
+      .then(async () => {
+        await Routine.findAll({
+          where: { id: routineId },
+          include: [
+            {
+              model: Action,
+              include: [
+                {
+                  model: ActionFin,
+                },
+              ],
             },
           ],
-        },
-      ],
-    }).catch((err) => {
-      if (err) return next(new Error('db 에러'));
-    });
+        }).then(async (target) => {
+          // console.log('완료되지 않은 액션의 개수', count);
+          // console.log('타겟루틴', target);
+          // console.log('타겟루틴속 액션', target[0].Actions);
+          // console.log('타겟루틴의 액션 개수', target[0].Actions.length);
+          // console.log('타겟루틴의 액션의 액션핀의 데이터값', target[0].Actions[0].ActionFins[0]);
+          // console.log('타겟루틴의 액션의 액션핀의 데이터값', target[0].Actions[0].ActionFins[0].date);
 
-    // console.log('완료되지 않은 액션의 개수', count);
-    // console.log('타겟루틴', target);
-    // console.log('타겟루틴속 액션', target[0].Actions);
-    // console.log('타겟루틴의 액션 개수', target[0].Actions.length);
-    // console.log('타겟루틴의 액션의 액션핀의 데이터값', target[0].Actions[0].ActionFins[0]);
-    // console.log('타겟루틴의 액션의 액션핀의 데이터값', target[0].Actions[0].ActionFins[0].date);
+          let count = 0
+          for(let i = 0; i < target[0].Actions.length; i++ ){
+            if( target[0].Actions[i].ActionFins[0].date == null ){
+              count = count + 1;
+            }
+            console.log(count);
+          }
+          console.log('밖의 카운트',count);
+          
+          // return res.send({
+          //   msg:'임시 멈춤',
+          // });
 
-    let count = 0;
-    for (let i = 0; i < target[0].Actions.length; i++) {
-      if (target[0].Actions[i].ActionFins[0].date == null) {
-        count = count + 1;
-      }
-      console.log(count);
-    }
-    console.log('밖의 카운트', count);
+          //Action들 속 ActionFin의 Date가 null인 것의 개수가 0보다 크다 === 루틴에 완료되지 않은 액션이 있다.
+          if (count > 0) {
+            console.log('액션만 완료된 경우');
 
-    //Action들 속 ActionFin의 Date가 null인 것의 개수가 0보다 크다 === 루틴에 완료되지 않은 액션이 있다.
-    if (count > 0) {
-      console.log('액션만 완료된 경우');
+            //5. 액션이 완료되었을때 경험치
+            if (await upExpFinOneAction(userId)) {
+              console.log('원액션 표시표시');
+              return res.status(200).send({
+                result: 'true1',
+                msg: 'Action 완료 날짜 표시, 경험치 지급 완료',
+              });
+            } else {
+              console.log('원액션 표시표시');
+              return res.status(200).send({
+                result: 'true3',
+                msg: 'Action 완료 날짜 표시, 경험치 지급 불가 ( 사유 : 일일 한계 초과 )',
+              });
+            }
+          } else {
+            console.log('액션과 루틴이 함께 완료된 경우');
+            //5. 루틴에는 finDate추가
+            setRoutineFinDate(routineId, finDate).catch((err) => {
+              if (err) return next(new Error('setRoutineFinDate db 에러'));
+            });
 
-      //5. 액션이 완료되었을때 경험치
-      if (await upExpFinOneAction(userId)) {
-        console.log('원액션 표시표시');
-        return res.status(200).send({
-          result: 'true1',
-          msg: 'Action 완료 날짜 표시, 경험치 지급 완료',
+            //6. 액션과 루틴이 함께 완료되었을때 경험치
+            if (await upExpAllAction(userId, routineId)) {
+              return res.status(200).send({
+                result: 'true2',
+                msg: 'Action과 Routine 완료 날짜 표시, 경험치 지급 완료',
+              });
+            } else {
+              return res.status(201).send({
+                result: 'true4',
+                msg: 'Action과 Routine 완료 날짜 표시, 경험치 지급 불가 ( 사유 : 일일 한계 초과 )',
+              });
+            }
+          }
         });
-      } else {
-        console.log('원액션 표시표시');
-        return res.status(200).send({
-          result: 'true3',
-          msg: 'Action 완료 날짜 표시, 경험치 지급 불가 ( 사유 : 일일 한계 초과 )',
-        });
-      }
-    } else {
-      console.log('액션과 루틴이 함께 완료된 경우');
-      //5. 루틴에는 finDate추가
-      setRoutineFinDate(routineId, finDate).catch((err) => {
-        if (err) return next(new Error('setRoutineFinDate db 에러'));
+      })
+      .catch((err) => {
+        console.log(err);
+        return next(myError(400, err.message));
       });
-
-      //6. 액션과 루틴이 함께 완료되었을때 경험치
-      if (await upExpAllAction(userId, routineId)) {
-        return res.status(200).send({
-          result: 'true2',
-          msg: 'Action과 Routine 완료 날짜 표시, 경험치 지급 완료',
-        });
-      } else {
-        return res.status(201).send({
-          result: 'true4',
-          msg: 'Action과 Routine 완료 날짜 표시, 경험치 지급 불가 ( 사유 : 일일 한계 초과 )',
-        });
-      }
-    }
   } catch (err) {
     console.log(err);
     return next(myError(400, err.message));
