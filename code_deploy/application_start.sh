525ed2da420afb52fdafc97e5282990d03814eb6
cxz5309 <cxz5309@gmail.com>
2021-11-08 16:21:22 +0900

    김영우 : pm2 실행 에러 해결

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -2,7 +2,6 @@
 sudo chmod -R 777 /home/ubuntu/mingijuk
 # delete all servers and start the server as a daemon
 cd /home/ubuntu/mingijuk
-sudo npm install -g pm2
-sudo pm2_home=/home/ubuntu/.pm2 pm2 kill
-sudo pm2_home=/home/ubuntu/.pm2 pm2 start npm -- start
-sudo pm2_home=/home/ubuntu/.pm2 pm2 save
+pkill -9 pm2
+pm2 start npm -- start
+pm2 save
