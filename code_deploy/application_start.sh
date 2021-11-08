04d59d6152e0f53c425944cc8bba5f8e6e7124ce
cxz5309 <cxz5309@gmail.com>
2021-11-08 16:07:22 +0900

    김영우 : pm2 update 버그 수정

--- code_deploy/application_start.sh
+++ code_deploy/application_start.sh
@@ -5,4 +5,3 @@ cd /home/ubuntu/app
 sudo pm2_home=/home/ubuntu/.pm2 pm2 kill
 sudo pm2_home=/home/ubuntu/.pm2 pm2 start npm -- start
 sudo pm2_home=/home/ubuntu/.pm2 pm2 save
-sudo pm2_home=/home/ubuntu/.pm2 pm2 update
