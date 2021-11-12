f4f9d52a8e60560dbb15ffa2b6026f6dda022ec5
Andrew <seongchan24@gmail.com>
2021-11-12 15:17:37 +0900

    임성찬: 소켓 작업 진행 중 백업

--- testFn.js
+++ testFn.js
@@ -1,6 +1,9 @@
 // //let characterFool = ['1', '2', '3'];
 // let crtFool = ['1', '2', '3'];
 
+const { compareSync } = require("bcrypt");
+const { calendarFormat } = require("moment");
+
 // //let result = characterFool[Math.floor(Math.random()*characterFool.length)];
 // let newcrtInfo1 = [Math.floor(Math.random()*crtFool.length)]; // = 인덱스 출력
 // let newcrtInfo = crtFool[Math.floor(Math.random()*crtFool.length)]; // 인덱스를 fool에 적용시켜서 값을 출력
@@ -10,26 +13,43 @@
 // console.log(newcrtInfo);
 
 
-crtPreSetList = ['asd', '라이언', '어피치'];
-collcetionMaxCrt = ['라이언'];
+// crtPreSetList = ['asd', '라이언', '어피치'];
+// collcetionMaxCrt = ['라이언'];
 
-// collcetionMaxCrt.sort();
-// crtPreSetList.sort();
-// let notCollection = []
-// for(let i=0; i<crtPreSetList.length; i++){
-//   if( collcetionMaxCrt[i] !== crtPreSetList[i]){
-//     console.log(crtPreSetList[i], "비교1");
-//     console.log(collcetionMaxCrt[i], "비교2");
+// // collcetionMaxCrt.sort();
+// // crtPreSetList.sort();
+// // let notCollection = []
+// // for(let i=0; i<crtPreSetList.length; i++){
+// //   if( collcetionMaxCrt[i] !== crtPreSetList[i]){
+// //     console.log(crtPreSetList[i], "비교1");
+// //     console.log(collcetionMaxCrt[i], "비교2");
 
-//     notCollection.push(crtPreSetList[i]);
-//     console.log(notCollection);
-//   }
+// //     notCollection.push(crtPreSetList[i]);
+// //     console.log(notCollection);
+// //   }
+// // };
+
+// let result = crtPreSetList.filter(x => !collcetionMaxCrt.includes(x));
+// console.log(result);
+
+// //인덱스뽑아오기
+// const asdqwe = '어피치';
+// const targetIndex = crtPreSetList.indexOf(asdqwe, 1);
+// console.log(targetIndex);
+
+
+
+// let a = 1;
+// let b = 4;
+// let result;
+
+// cal(a, b, result);
+
+// console.log(result);
+
+// function cal(val1, val2, r) {
+//   r = val1 + val2;
+//   console.log(r);
+//   result = r
+//   return;
 // };
-
-let result = crtPreSetList.filter(x => !collcetionMaxCrt.includes(x));
-console.log(result);
-
-//인덱스뽑아오기
-const asdqwe = '어피치';
-const targetIndex = crtPreSetList.indexOf(asdqwe, 1);
-console.log(targetIndex);
