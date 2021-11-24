a060082cbf31e059f99ee243d69b5fe065110b68
Andrew <seongchan24@gmail.com>
2021-11-24 20:03:47 +0900

    fix: DB 초기화

--- routes/moims.js
+++ routes/moims.js
@@ -6,18 +6,18 @@ const { createLike, getLikedMoims, deleteLike } = require('../controllers/likes'
 const { getAllComments, getTargetMoimComments, createComment, updateComment, deleteComment, myComments } = require('../controllers/comments');
 
 //API
-router.get('/like/mylikes', authMiddleware, getLikedMoims); //테스트
+router.get('/like/mylikes', authMiddleware, getLikedMoims);
 router.post('/like/:moimId', authMiddleware, createLike);
 router.delete('/like/:moimId', authMiddleware, deleteLike);
 
-router.get('/comment/mycomments', authMiddleware, myComments); //테스트
+router.get('/comment/mycomments', authMiddleware, myComments);
 router.get('/comment/:moimId', authMiddleware, getTargetMoimComments); //특정 모임에 대한 댓글 불러오기
 router.post('/comment/:moimId', authMiddleware, createComment);
 router.put('/comment/:commentId', authMiddleware, updateComment);
 router.delete('/comment/:commentId', authMiddleware, deleteComment);
 router.get('/comment/', authMiddleware, getAllComments);
 
-router.post('/mymoims', authMiddleware, myMoims); //테스트
+router.post('/mymoims', authMiddleware, myMoims);
 router.post('/search', getMoimByLocation); //미들웨어 제거
 router.post('/:moimId/exit', authMiddleware, exitMoim);
 router.get('/:moimId', authMiddleware, detailMoim);
