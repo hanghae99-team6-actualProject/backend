44966fa83854bdb503814fd3c3908cc871c7e70c
Andrew <seongchan24@gmail.com>
2021-11-27 01:53:13 +0900

    fix: 루틴 및 액션의 종속관계 제거

--- models/routinefin.js
+++ models/routinefin.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.RoutineFin.belongsTo(models.Routine, { foreignKey: 'routineId', targetKey: 'id' });
+
       models.RoutineFin.hasMany(models.ActionFin, { foreignKey: 'routineFinId', sourceKey: 'id' });
     }
   };
