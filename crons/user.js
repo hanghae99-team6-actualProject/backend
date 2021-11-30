65aba15ebcd204a60c7fae5017128392ab8179d8
Jason <joohyuk.yang00@gmail.com>
2021-11-30 22:23:20 +0900

    feat: cron softdelete->harddelete 기준 수정(매일 자정, 탈퇴 후 한달 초과한 유저 삭제)

--- crons/user.js
+++ crons/user.js
@@ -2,18 +2,33 @@ const cron = require('node-cron');
 const { User, Sequelize } = require("../models");
 const Op = Sequelize.Op;
 
+function timeSet() {
+  const today = new Date();
+  const year = today.getFullYear(); // 년도
+  const month = today.getMonth(); // 월
+  const date = today.getDate(); // 날짜
+  const fromMonthAgo = new Date(year, month - 1, date, 0, 0, 0);
+
+  return { today, fromMonthAgo };
+}
+
 const destroyUser = () => {
-  cron.schedule('*/1 * * * *', function () {
-    User.destroy({
-      where: {
-        deletedAt: {
-          [Op.not]: null,
-          
-        }
-      },
-      force: true
-    });
-    console.log('한 시간 마다 작업 실행 :', new Date().toString());
+  cron.schedule('0 0 * * *', function () {
+    try {
+      const { fromMonthAgo, today } = timeSet();
+      User.destroy({
+        where: {
+          deletedAt: {
+            [Op.not]: null,
+            [Op.notBetween]: [fromMonthAgo, today],
+          }
+        },
+        force: true
+      });
+      ogger.info('매일 자정마다 실행 :', new Date().toString());
+    } catch (error) {
+      ogger.info(error);
+    }
   });
 }
 
